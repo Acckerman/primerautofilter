5f17a018f24f61bbe6a792c9c9e364a25250fe12
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-02-05 14:58:53 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('𝙷𝙾𝚆 𝚃𝙾 𝚄𝚂𝙴 𝙼𝙴', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/LazyDeveloper')
+            InlineKeyboardButton('📢 UPDATES 📢', url='https://t.me/k2m_movies_series')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
