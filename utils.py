b1b615848e7f03bd82b4f32317c0b4e2643b928b
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-02-12 14:57:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -394,7 +394,7 @@ async def get_shortlink(link):
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
                 data = await response.json()
                 if data["status"] == "success":
-                    return data['shortenedurl']
+                    return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
                     return f'https://dulink.in/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
