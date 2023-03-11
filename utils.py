0033d4d4ada23d617b98408cfdd848b9f5c1800e
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-03-11 15:19:13 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'http://tnlink.in/api'
+    url = f'http://TinyFy.in'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
                }
